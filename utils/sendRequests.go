40bc4148bfca2e41abac01cad28356851548d548
Sugam Panthi <man359905@gmail.com>
2024-11-30 14:29:57 -0600

    Created Send Helper

---
+++ utils/sendRequests.go
@@ -0,0 +1,18 @@
+package Utils
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func SendRequest(req *http.Request) (*http.Response, error) {
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	return resp, nil
+}
