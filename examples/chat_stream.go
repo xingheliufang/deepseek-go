ee5a24a0e667e4b657ff6362e396490a6f9313e8
Sugam Panthi <man359905@gmail.com>
2024-12-01 19:34:36 -0600

    Implemented Streaming Example.

---
+++ examples/chat_stream.go
@@ -0,0 +1,48 @@
+package deepseek_examples
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	deepseek "github.com/cohesion-org/deepseek-go"
+)
+
+func TestStream(t *testing.T) {
+	client := deepseek.NewClient(os.Getenv("DEEPSEEK_KEY"))
+	request := &deepseek.StreamChatCompletionRequest{
+		Model: deepseek.DeepSeekChat,
+		Messages: []deepseek.ChatCompletionMessage{
+			{Role: deepseek.ChatMessageRoleUser, Content: "Just testing if the streaming feature is working or not!"},
+		},
+		Stream: true,
+	}
+	ctx := context.Background()
+
+	stream, err := client.CreateChatCompletionStream(ctx, request)
+	if err != nil {
+		log.Fatalf("ChatCompletionStream error: %v", err)
+	}
+	var fullMessage string
+	defer stream.Close()
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("\nStream finished")
+			break
+		}
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			break
+		}
+		for _, choice := range response.Choices {
+			fullMessage += choice.Delta.Content // Accumulate chunk content
+			log.Println(choice.Delta.Content)
+		}
+	}
+	log.Println("The full message is: ", fullMessage)
+}
