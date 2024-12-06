d098a5a50e76279bdc108058f8915a6e582f5b5b
Sugam Panthi <man359905@gmail.com>
2024-12-05 19:08:05 -0600

    #4 Added new parameters for the steramoptions. Todo: Debug them.

--- chat_stream.go
+++ chat_stream.go
@@ -16,9 +16,19 @@ type StreamChatCompletionMessage struct {
 }
 
 type StreamChatCompletionRequest struct {
-	Model    string                  `json:"model"`
-	Messages []ChatCompletionMessage `json:"messages"`
-	Stream   bool                    `json:"stream,omitempty"`
+	Stream           bool                    `json:"stream,omitempty"`            //Comments: Defaults to true, since it's "STREAM"
+	Model            string                  `json:"model"`                       // Required: Model ID, e.g., "deepseek-chat"
+	Messages         []ChatCompletionMessage `json:"messages"`                    // Required: List of messages
+	FrequencyPenalty float32                 `json:"frequency_penalty,omitempty"` // Optional: Frequency penalty, >= -2 and <= 2
+	MaxTokens        int                     `json:"max_tokens,omitempty"`        // Optional: Maximum tokens, > 1
+	PresencePenalty  float32                 `json:"presence_penalty,omitempty"`  // Optional: Presence penalty, >= -2 and <= 2
+	Temperature      float32                 `json:"temperature,omitempty"`       // Optional: Sampling temperature, <= 2
+	TopP             float32                 `json:"top_p,omitempty"`             // Optional: Nucleus sampling parameter, <= 1
+	ResponseFormat   *ResponseFormat         `json:"response_format,omitempty"`   // Optional: Custom response format: just don't try, it breaks rn ;)
+	Stop             []string                `json:"stop,omitempty"`              // Optional: Stop signals
+	Tools            []Tools                 `json:"tools,omitempty"`             // Optional: List of tools
+	LogProbs         bool                    `json:"logprobs,omitempty"`          // Optional: Enable log probabilities
+	TopLogProbs      int                     `json:"top_logprobs,omitempty"`      // Optional: Number of top tokens with log probabilities, <= 20
 }
 
 type ChatCompletionStream interface {
