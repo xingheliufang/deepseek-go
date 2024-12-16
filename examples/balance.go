7e3e06956023d6af9b94eb84d0b57e3ae73c9161
Sugam Panthi <man359905@gmail.com>
2024-12-15 18:55:03 -0600

    Add example: Balance.go

---
+++ examples/balance.go
@@ -0,0 +1,27 @@
+package deepseek_examples
+
+import (
+	"context"
+	"log"
+	"os"
+
+	deepseek "github.com/cohesion-org/deepseek-go"
+)
+
+func Balance() {
+	client := deepseek.NewClient(os.Getenv("DEEPSEEK_KEY"))
+	ctx := context.Background()
+	balance, err := deepseek.GetBalance(client, ctx)
+	if err != nil {
+		log.Fatalf("Error getting balance: %v", err)
+	}
+
+	if balance == nil {
+		log.Fatalf("Balance is nil")
+	}
+
+	if len(balance.BalanceInfos) == 0 {
+		log.Fatalf("No balance information returned")
+	}
+	log.Printf("%+v\n", balance)
+}
