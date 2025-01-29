3308c814062f25fce59fdb1faaf2f176e0b2a727
Sugam Panthi <man359905@gmail.com>
2025-01-29 08:57:36 -0600

    add: Created a new List All models function.

--- models.go
+++ models.go
@@ -1,8 +1,64 @@
 package deepseek
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	handlers "github.com/cohesion-org/deepseek-go/handlers"
+	utils "github.com/cohesion-org/deepseek-go/utils"
+)
+
 // Create chat and coder models here!
+
 const (
 	DeepSeekChat     = "deepseek-chat"
 	DeepSeekCoder    = "deepseek-coder"
 	DeepSeekReasoner = "deepseek-reasoner"
 )
+
+type Model struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	OwnedBy string `json:"owned_by"`
+}
+
+type APIModels struct {
+	Object string  `json:"object"`
+	Data   []Model `json:"data"`
+}
+
+// Models supported by the API itself
+func ListALLModels(c *Client, ctx context.Context) (*APIModels, error) {
+	req, err := utils.NewRequestBuilder(c.AuthToken).
+		SetBaseURL("https://api.deepseek.com/").
+		SetPath("models").
+		BuildGet(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("error building request: %w", err)
+	}
+
+	resp, err := handlers.HandelNormalRequest(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return nil, HandleAPIError(resp)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var models APIModels
+	if err := json.Unmarshal(body, &models); err != nil {
+		return nil, fmt.Errorf("failed to parse response JSON: %w", err)
+	}
+	return &models, nil
+}
