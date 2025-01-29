104d09282425cd09bb83c18b95ae380faa7724bd
Sugam Panthi <man359905@gmail.com>
2025-01-29 08:56:44 -0600

    Added a Normal Request with reduced timeout.

--- handlers/requestHandler.go
+++ handlers/requestHandler.go
@@ -16,3 +16,14 @@ func HandleSendChatCompletionRequest(req *http.Request) (*http.Response, error) 
 	}
 	return resp, nil
 }
+
+func HandelNormalRequest(req *http.Request) (*http.Response, error) {
+	client := &http.Client{
+		Timeout: 20 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	return resp, nil
+}
