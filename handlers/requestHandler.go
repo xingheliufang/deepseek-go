aa5f405a465a72ba177c01c29c9239bbeedc7066
Sakshyam Sigdel <sakshyamsigdel@gmail.com>
2025-01-30 15:07:38 -0600

    Added dynamic timeout, parsed through dot env

--- handlers/requestHandler.go
+++ handlers/requestHandler.go
@@ -4,26 +4,47 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+  "os"
 )
+func HandleTimeout() (time.Duration, error){
+	if timeout := os.Getenv("DEEPSEEK_TIMEOUT"); timeout != "" {
+		if d, err := time.ParseDuration(timeout); err == nil {
+			timeout = d
+      return timeout, nil
+		} 
+	}
+    return time.ParseDuration("5m"), err //Default timeout behavior if no timeout is set
+}
 
 func HandleSendChatCompletionRequest(req *http.Request) (*http.Response, error) {
+  timeout, err = HandleTimeout()
+  if err != nil{
+    return nil, fmt.Errorf("Error sending request %w", err)
+  }
 	client := &http.Client{
-		Timeout: 240 * time.Second,
+    Timeout: timeout,
 	}
 	resp, err := client.Do(req)
-	if err != nil {
+	if err != nil{
 		return nil, fmt.Errorf("error sending request: %w", err)
 	}
 	return resp, nil
 }
 
 func HandelNormalRequest(req *http.Request) (*http.Response, error) {
+  timeout, err = HandleTimeout()
+  if err != nil{
+    return nil, fmt.Errorf("Error sending request %w", err)
+  }
 	client := &http.Client{
-		Timeout: 20 * time.Second,
+		Timeout: timeout,
 	}
 	resp, err := client.Do(req)
-	if err != nil {
+	if err !=nil{
 		return nil, fmt.Errorf("error sending request: %w", err)
 	}
 	return resp, nil
+}
+
+
 }
