1f941646251026149ff0f4536543c8b32e349799
Sugam Panthi <man359905@gmail.com>
2025-02-04 15:10:43 -0600

    add: #22 added the requested changes!

--- handlers/requestHandler.go
+++ handlers/requestHandler.go
@@ -1,23 +1,42 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func HandleTimeout() (time.Duration, error) {
+	if err := godotenv.Load(); err != nil {
+		_ = err
+	}
+
 	timeoutStr := os.Getenv("DEEPSEEK_TIMEOUT")
 	if timeoutStr == "" {
 		return 5 * time.Minute, nil
 	}
 	duration, err := time.ParseDuration(timeoutStr)
 	if err != nil {
-		// Return error with context if parsing fails
 		return 0, fmt.Errorf("invalid timeout duration %q: %w", timeoutStr, err)
 	}
 	return duration, nil
+}
+
+func checkTimeoutError(err error, timeout time.Duration) error {
+	var urlErr *url.Error
+	if errors.As(err, &urlErr) && urlErr.Timeout() {
+		return fmt.Errorf(
+			"request timed out after %s. You can increase the timeout by setting the DEEPSEEK_TIMEOUT environment variable. Original error: %w",
+			timeout,
+			err,
+		)
+	}
+	return nil
 }
 
 func HandleSendChatCompletionRequest(req *http.Request) (*http.Response, error) {
@@ -25,12 +44,13 @@ func HandleSendChatCompletionRequest(req *http.Request) (*http.Response, error) 
 	if err != nil {
 		return nil, fmt.Errorf("error getting timeout: %w", err)
 	}
-	client := &http.Client{
-		Timeout: timeout,
-	}
 
+	client := &http.Client{Timeout: timeout}
 	resp, err := client.Do(req)
 	if err != nil {
+		if timeoutErr := checkTimeoutError(err, timeout); timeoutErr != nil {
+			return nil, timeoutErr
+		}
 		return nil, fmt.Errorf("error sending request: %w", err)
 	}
 
@@ -43,12 +63,12 @@ func HandleNormalRequest(req *http.Request) (*http.Response, error) {
 		return nil, fmt.Errorf("error getting timeout: %w", err)
 	}
 
-	client := &http.Client{
-		Timeout: timeout,
-	}
-
+	client := &http.Client{Timeout: timeout}
 	resp, err := client.Do(req)
 	if err != nil {
+		if timeoutErr := checkTimeoutError(err, timeout); timeoutErr != nil {
+			return nil, timeoutErr
+		}
 		return nil, fmt.Errorf("error sending request: %w", err)
 	}
 
