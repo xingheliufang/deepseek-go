86a6628de96b3320b6226bd840f47b85e5efb6eb
Sugam Panthi <man359905@gmail.com>
2025-02-11 16:08:59 -0600

    #27 add: examples for using the FIM feature.

---
+++ examples/fim.go
@@ -0,0 +1,24 @@
+package deepseek_examples
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	deepseek "github.com/cohesion-org/deepseek-go"
+)
+
+func FIM() {
+	client := deepseek.NewClient(os.Getenv("DEEPSEEK_API_KEY"))
+	request := &deepseek.FIMCompletionRequest{
+		Model:  deepseek.DeepSeekChat,
+		Prompt: "def add(a, b):",
+	}
+	ctx := context.Background()
+	response, err := client.CreateFIMCompletion(ctx, request)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Println("\n", response.Choices[0].Text)
+}
