74465070feb83af99250ea84ea81444f32e3728e
Sugam Panthi <man359905@gmail.com>
2025-02-14 09:57:55 -0600

    refactor: update comments for message roles in constants package

--- constants/constants.go
+++ constants/constants.go
@@ -1,11 +1,11 @@
+// package constants contains the constants used in the deepseek-go package
 package constants
 
 const (
-	// @unused-constants
-	EndpointCompletion = "/chat/completions"
-	EndpointSearch     = "/search"
-
-	ChatMessageRoleSystem    = "system"
-	ChatMessageRoleUser      = "user"
+	// ChatMessageRoleSystem is the role of a system message
+	ChatMessageRoleSystem = "system"
+	// ChatMessageRoleUser is the role of a user message
+	ChatMessageRoleUser = "user"
+	// ChatMessageRoleAssistant is the role of an assistant message
 	ChatMessageRoleAssistant = "assistant"
 )
