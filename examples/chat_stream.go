af2ebeebdf118501a5b6424fbdaf6268691fa0a6
Sugam Panthi <man359905@gmail.com>
2025-02-14 10:20:30 -0600

    feat: add StreamingWithReasoningContent function to handle chat completion with reasoning

--- examples/chat_stream.go
+++ examples/chat_stream.go
@@ -46,3 +46,52 @@ func Streaming() {
 	}
 	log.Println("The full message is: ", fullMessage)
 }
+
+// streaing with ReasoningContent
+func StreamingWithReasoningContent() {
+	client := deepseek.NewClient(os.Getenv("DEEPSEEK_API_KEY"))
+	request := &deepseek.StreamChatCompletionRequest{
+		Model: deepseek.DeepSeekReasoner,
+		Messages: []deepseek.ChatCompletionMessage{
+			{Role: constants.ChatMessageRoleUser, Content: "Hello, how are you?"},
+		},
+		Stream: true,
+	}
+	ctx := context.Background()
+
+	stream, err := client.CreateChatCompletionStream(ctx, request)
+	if err != nil {
+		log.Fatalf("ChatCompletionStream error: %v", err)
+	}
+
+	var fullMessage string
+	var fullReasoning string
+	defer stream.Close()
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("\nStream finished")
+			break
+		}
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			break
+		}
+		for _, choice := range response.Choices {
+			fullMessage += choice.Delta.Content            // Accumulate chunk content
+			fullReasoning += choice.Delta.ReasoningContent // Accumulate chunk reasoning content
+			if choice.Delta.ReasoningContent != "" {
+				log.Println("Reasoning: ", choice.Delta.ReasoningContent)
+			}
+			if choice.Delta.Content != "" {
+				log.Println("Content:", choice.Delta.Content)
+			}
+		}
+		if streamUsage := response.Usage; streamUsage != nil && streamUsage.TotalTokens > 0 {
+			log.Printf("Prompt tokens: %d, Completion tokens: %d, Total tokens: %d",
+				streamUsage.PromptTokens, streamUsage.CompletionTokens, streamUsage.TotalTokens)
+		}
+	}
+	log.Println("Full message: ", fullMessage)
+	log.Println("\nFull reasoning: ", fullReasoning)
+}
